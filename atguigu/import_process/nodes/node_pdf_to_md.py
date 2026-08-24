44315a311da9ce743364a22c9fc12f2dffc7d68c
zhizhu1 <2293686334@qq.com>
2026-08-24 11:14:17 +0800

    1

---
+++ atguigu/import_process/nodes/node_pdf_to_md.py
@@ -0,0 +1,15 @@
+# atguigu/import_process/nodes/node_pdf_to_md.py
+from atguigu.import_process.base import NodeBase
+from atguigu.import_process.state import ImportGraphState
+
+
+class NodePDFToMD(NodeBase):
+    """
+    PDF 转 Markdown 节点：PDF结构化解析
+    """
+
+    name = "node_pdf_to_md"
+
+    def process(self, state: ImportGraphState):
+
+        return state
